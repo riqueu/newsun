418c939173efb858ae80fea4938eee2db174d6a1
Brunikito <brunoluiszrosa@gmail.com>
2024-11-08 10:08:11 -0300

    First test

--- main.py
+++ main.py
@@ -1,126 +1,70 @@
 import pygame
+from src.config import *
+from src.sprites import *
 import sys
-import random
-import json
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-# Inicialização
-pygame.init()
-screen_width, screen_height = 900, 500
-screen = pygame.display.set_mode((screen_width, screen_height))
-clock = pygame.time.Clock()
+        self.character_spritesheet = SpriteSheet('img/character.png')
 
-# Game
-COLLISION = False
+    
+    def draw_player(self):
+        Quarto(self, (WIN_WIDTH-ROOM_WIDHT)//(2*TILESIZE), (WIN_HEIGHT-ROOM_HEIGH)//(2*TILESIZE))
+        Player(self, (WIN_WIDTH-ROOM_WIDHT)//(2*TILESIZE) + 4, (WIN_HEIGHT-ROOM_HEIGH)//(2*TILESIZE) + 7)
 
-# Jogador
-player_size = 50
-player_color = (255, 0, 0)
-player_pos = [screen_width // 2, screen_height // 2]  # Começa no centro da tela
-speed = 1.5
-player_word = [800,400]
+    def new(self):
+        self.playing = True
 
-# Read json maps
-with open('mapas.json', 'r') as arquivo:
-	mapas = json.load(arquivo)
-	
-# player
-player_import = pygame.image.load("player.png")
-palyer = pygame.transform.scale(player_import, (player_size, player_size))
-# mapa
-background_import = pygame.image.load(mapas["quarto"]["img"])
-background = pygame.transform.scale(background_import, (900, 500))
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.atacks = pygame.sprite.LayeredUpdates()
 
-# # Função de detecção de colisão
-def check_collision(player_rect, obstacles):
-	for obstacle in obstacles:
-		if player_rect.colliderect(obstacle["rect"]):
-			return True
-	return False
+        self.draw_player()
+    
+    def events(self):
+        # game loop events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
 
-# Função para adicionar obstáculos
-def add_square(position, dimension):
-	return pygame.Rect(position["x"], position["y"], dimension[0], dimension[1])
+    def update(self):
+        # game loop updates
+        self.all_sprites.update()
 
-quarto_obstacles = mapas["quarto"]["obstacles"]
+    def draw(self):
+        # game loop draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
 
-# Adiciona obstáculos a partir do JSON
-obstacles = []
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
 
-for obstacles_type in quarto_obstacles:
-	print("obstacles_type: ", obstacles_type)
-	for obstacle in quarto_obstacles[obstacles_type]:
-		print("obstacle: ", obstacle)
-		rect = add_square(obstacle["position"], (obstacle["width"], obstacle["height"]))
-		
-		obs = {
-			"type": obstacles_type,
-			"rect": rect,
-		}
+    def game_over(self):
+        pass
 
-		if "border_radius" in obstacle:
-			obs["border_radius"] = obstacle["border_radius"]
+    def intro_screen(self):
+        pass
 
-		obstacles.append(obs)
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
 
-# Loop principal
-running = True
-while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()	
-
-	# Controle do Jogador
-	keys = pygame.key.get_pressed()
-	player_movement = [0, 0]
-
-	if keys[pygame.K_LEFT]:
-		player_movement[0] -= speed
-	if keys[pygame.K_RIGHT]:
-		player_movement[0] += speed
-	if keys[pygame.K_UP]:
-		player_movement[1] -= speed
-	if keys[pygame.K_DOWN]:
-		player_movement[1] += speed
-
-	# Calcular nova posição do jogador no mundo
-	player_word_test = [player_word[0] + player_movement[0], player_word[1] + player_movement[1]]
-	player_rect_test = pygame.Rect(player_word_test[0]+7, player_word_test[1]+32, player_size-20, player_size-30)
-
-	
-	# Verificar colisão
-	if check_collision(player_rect_test, obstacles):
-		continue
-	
-	# Seta a nova posição do jogador no mundo
-	player_word = [player_word[0] + player_movement[0], player_word[1] + player_movement[1]]
-
-	# Desenhar retângulo ao redor do jogador para mostrar a área de colisão
-	player_rect = pygame.Rect(player_word[0], player_word[1], player_size, player_size)
-	pygame.draw.rect(screen, (0, 0, 255), player_rect, 2)  # Borda azul ao redor do jogador
-
-	# Limpar a tela
-	# screen.fill(mapas.quarto.src)  # Verde para o fundo
-	screen.blit(background, (0,0))
-		
-	screen.blit(palyer, player_word)
-	
-	if COLLISION:
-		for obstacle in obstacles:
-			# print(obstacle)
-			if obstacle["type"] == "wall":
-				if "border_radius" in obstacle:
-					pygame.draw.rect(screen, (255, 0, 0), obstacle["rect"], 3, border_radius=obstacle["border_radius"])
-				else:
-					pygame.draw.rect(screen, (255, 0, 0), obstacle["rect"], 3)
-				
-			elif obstacle["type"] == "object":
-				pygame.draw.rect(screen, (255, 255, 0), obstacle["rect"], 3, border_radius=5)
-
-		pygame.draw.rect(screen, player_color, player_rect_test, 1, border_radius=10)
-
-
-	# Atualizar a tela
-	pygame.display.flip()
-	clock.tick(60)  # 60 FPS
+pygame.quit()
+sys.exit()
---
+++ src/sprites.py
@@ -0,0 +1,189 @@
+import pygame
+from src.config import *
+import math
+import random
+
+class SpriteSheet():
+    def __init__(self, file):
+        self.sheet = pygame.image.load(file).convert()
+
+    def get_sprite(self, x, y, widht, height):
+        sprite = pygame.Surface([widht, height])
+        sprite.blit(self.sheet, (0,0), (x, y, widht, height))
+        sprite.set_colorkey(BLACK)
+        return sprite
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+
+        self.game = game
+        self._layer = PLAYER_LAYER
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.height = TILESIZE
+
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = 'down'
+        self.animation_loop = 1
+
+        self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
+        
+        self.image_hitbox = pygame.image.load('img/player_hitbox.png').convert_alpha()
+
+        self.mask = pygame.mask.from_surface(self.image_hitbox)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+    
+    def update(self):
+        previous_x = self.rect.x
+        previous_y = self.rect.y
+
+        # Executar o movimento do jogador
+        self.movement()
+        self.animate()
+
+        # Tentar mover horizontalmente, se possível
+        self.rect.x += self.x_change
+        if self.colisao_com_paredes_horizontal(self.game.all_sprites):
+            self.rect.x = previous_x  # Reverter o movimento horizontal se houver colisão
+
+        # Tentar mover verticalmente, se possível
+        self.rect.y += self.y_change
+        if self.colisao_com_paredes_vertical(self.game.all_sprites):
+            self.rect.y = previous_y  # Reverter o movimento vertical se houver colisão
+
+        # Resetar as mudanças no movimento após cada atualização
+        self.x_change = 0
+        self.y_change = 0
+
+    def animate(self):
+        down_animations = [self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(35, 2, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(68, 2, self.width, self.height)]
+
+        up_animations = [self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height),
+                         self.game.character_spritesheet.get_sprite(35, 34, self.width, self.height),
+                         self.game.character_spritesheet.get_sprite(68, 34, self.width, self.height)]
+
+        left_animations = [self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(35, 98, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(68, 98, self.width, self.height)]
+
+        right_animations = [self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height),
+                            self.game.character_spritesheet.get_sprite(35, 66, self.width, self.height),
+                            self.game.character_spritesheet.get_sprite(68, 66, self.width, self.height)]
+        
+        if self.facing == 'down':
+            if self.y_change == 0:
+                self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
+            else:
+                self.image = down_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 3:
+                    self.animation_loop = 1
+        
+        if self.facing == 'up':
+            if self.y_change == 0:
+                self.image = self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height)
+            else:
+                self.image = up_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 3:
+                    self.animation_loop = 1
+        
+        if self.facing == 'left':
+            if self.x_change == 0:
+                self.image = self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height)
+            else:
+                self.image = left_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 3:
+                    self.animation_loop = 1
+        
+        if self.facing == 'right':
+            if self.x_change == 0:
+                self.image = self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height)
+            else:
+                self.image = right_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1
+                if self.animation_loop >= 3:
+                    self.animation_loop = 1
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            # for sprite in self.game.all_sprites:
+            #     sprite.rect.x += PLAYER_SPEED
+            self.x_change -= PLAYER_SPEED
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            # for sprite in self.game.all_sprites:
+            #     sprite.rect.x -= PLAYER_SPEED
+            self.x_change += PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            # for sprite in self.game.all_sprites:
+            #     sprite.rect.y += PLAYER_SPEED
+            self.y_change -= PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            # for sprite in self.game.all_sprites:
+            #     sprite.rect.y -= PLAYER_SPEED
+            self.y_change += PLAYER_SPEED
+            self.facing = 'down'
+
+
+    def colisao_com_paredes_horizontal(self, outros_sprites):
+        for sprite in outros_sprites:
+            if isinstance(sprite, Quarto):  # Verifique colisão com o fundo do quarto
+                # Verifique colisões horizontais
+                offset = (self.rect.x - sprite.rect.x, self.rect.y - sprite.rect.y)
+                if sprite.mask.overlap(self.mask, offset):
+                    return True  # Colidiu com a parede horizontalmente
+        return False
+
+    def colisao_com_paredes_vertical(self, outros_sprites):
+        for sprite in outros_sprites:
+            if isinstance(sprite, Quarto):  # Verifique colisão com o fundo do quarto
+                # Verifique colisões verticais
+                offset = (self.rect.x - sprite.rect.x, self.rect.y - sprite.rect.y)
+                if sprite.mask.overlap(self.mask, offset):
+                    return True  # Colidiu com a parede verticalmente
+        return False
+
+
+class Quarto(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = GROUND_LAYER
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = ROOM_WIDHT
+        self.height = ROOM_HEIGH
+
+        self.image = pygame.image.load('img/room.png').convert_alpha()
+        self.image.set_colorkey(BLUE)
+
+        self.image_colision = pygame.image.load('img/room_colision.png').convert_alpha()
+        self.image_colision.set_colorkey(BLUE)
+
+        self.mask = pygame.mask.from_surface(self.image_colision)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+    def update(self):
+        # Atualiza o sprite do quarto se necessário
+        pass
