726e9e12a8f8ca86a6c30493c34f0de7525b53ed
Brunikito <brunoluiszrosa@gmail.com>
2024-11-16 12:48:17 -0300

    copiada a branch do gustavo

--- main.py
+++ main.py
@@ -10,20 +10,24 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.character_spritesheet = SpriteSheet('img/character.png')
-
+        self.character_spritesheet = SpriteSheet('./img/characters/characters.png')
+        self.camera = Camera(self, WIN_WIDTH, WIN_HEIGHT)
     
     def draw_player(self):
-        Quarto(self, (WIN_WIDTH-ROOM_WIDHT)//2, (WIN_HEIGHT-ROOM_HEIGH)//2)
-        Player(self, (WIN_WIDTH-ROOM_WIDHT)//2 + 100, (WIN_HEIGHT-ROOM_HEIGH)//2 + 200)
+        self.player = Player(self, (WIN_WIDTH-ROOM_WIDHT)//2 + 120, (WIN_HEIGHT-ROOM_HEIGHT)//2 + 240)
+
 
     def new(self):
         self.playing = True
 
         self.all_sprites = pygame.sprite.LayeredUpdates()
-        # self.blocks = pygame.sprite.LayeredUpdates()
-        # self.enemies = pygame.sprite.LayeredUpdates()
-        # self.atacks = pygame.sprite.LayeredUpdates()
+        self.doors = pygame.sprite.Group()
+
+        self.room = Room(self, (WIN_WIDTH-ROOM_WIDHT)//2, (WIN_HEIGHT-ROOM_HEIGHT)//2)
+        self.room_door = RoomDoor(self, self.room, 'down')
+        self.all_sprites.add(self.room)
+        self.doors.add(self.room_door)
+        self.all_sprites.add(self.room_door)
 
         self.draw_player()
     
@@ -33,15 +37,25 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
-
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_e:
+                    # Verifica se o jogador está perto de alguma porta
+                    for door in self.doors:
+                        if door.rect.colliderect(self.player.rect):  # Verifica se o jogador está dentro do alcance da porta
+                            door.interact_with_door()
+            
     def update(self):
         # game loop updates
         self.all_sprites.update()
+        self.camera.box_target_camera(self.player)
+        self.camera.keyboard_control()
 
     def draw(self):
         # game loop draw
         self.screen.fill((35, 14, 13))
-        self.all_sprites.draw(self.screen)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, sprite.rect.topleft - self.camera.offset)
         self.clock.tick(FPS)
         pygame.display.update()
 
@@ -58,6 +72,44 @@ class Game:
 
     def intro_screen(self):
         pass
+
+class Camera:
+    def __init__(self, game, width, height):
+        self.game = game
+        self.camera_rect = pygame.Rect(0, 0, width, height)
+        self.offset = pygame.Vector2(0, 0)
+        self.camera_borders = {'left': 100, 'right': 100, 'top': 200, 'bottom': 200}
+        self.keyboard_speed = 5
+
+    def box_target_camera(self, target):
+        if target.rect.left < self.camera_rect.left + self.camera_borders['left']:
+            self.camera_rect.left = target.rect.left - self.camera_borders['left']
+
+        # Verificar se o jogador ultrapassou a borda direita da tela
+        if target.rect.right > self.camera_rect.right - self.camera_borders['right']:
+            self.camera_rect.right = target.rect.right + self.camera_borders['right']
+
+        # Verificar se o jogador ultrapassou a borda superior da tela
+        if target.rect.top < self.camera_rect.top + self.camera_borders['top']:
+            self.camera_rect.top = target.rect.top - self.camera_borders['top']
+
+        # Verificar se o jogador ultrapassou a borda inferior da tela
+        if target.rect.bottom > self.camera_rect.bottom - self.camera_borders['bottom']:
+            self.camera_rect.bottom = target.rect.bottom + self.camera_borders['bottom']
+
+        # Atualiza o deslocamento da câmera para o movimento da tela
+        self.offset.x = self.camera_rect.left 
+        self.offset.y = self.camera_rect.top
+
+    def keyboard_control(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]: self.camera_rect.x -= self.keyboard_speed
+        if keys[pygame.K_d]: self.camera_rect.x += self.keyboard_speed
+        if keys[pygame.K_w]: self.camera_rect.y -= self.keyboard_speed
+        if keys[pygame.K_s]: self.camera_rect.y += self.keyboard_speed
+
+        self.offset.x = self.camera_rect.left
+        self.offset.y = self.camera_rect.top
 
 g = Game()
 g.intro_screen()
