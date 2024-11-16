b2706bbd87da8654f30405527d59a0ba738189ec
Brunikito <brunoluiszrosa@gmail.com>
2024-11-16 12:12:02 -0300

    Corrigi a câmera, adicionei troca de ambientes, alterei a sprite do player

--- main.py
+++ main.py
@@ -10,22 +10,58 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.character_spritesheet = SpriteSheet('C:\\Users\\guguo\\OneDrive\\Área de Trabalho\\FGV\\LP\\pygame\\newsun\\img\\character.png')
+        self.character_spritesheet = SpriteSheet('./img/characters/characters.png')
         self.camera = Camera(self, WIN_WIDTH, WIN_HEIGHT)
-
+    
     def draw_player(self):
-        Quarto(self, (WIN_WIDTH-ROOM_WIDHT)//2, (WIN_HEIGHT-ROOM_HEIGH)//2)
-        self.player = Player(self, (WIN_WIDTH-ROOM_WIDHT)//2 + 100, (WIN_HEIGHT-ROOM_HEIGH)//2 + 200)
+        self.player = Player(self, (WIN_WIDTH-ROOM_WIDHT)//2 + 120, (WIN_HEIGHT-ROOM_HEIGHT)//2 + 240)
+
 
     def new(self):
         self.playing = True
 
         self.all_sprites = pygame.sprite.LayeredUpdates()
-        # self.blocks = pygame.sprite.LayeredUpdates()
-        # self.enemies = pygame.sprite.LayeredUpdates()
-        # self.atacks = pygame.sprite.LayeredUpdates()
+        self.doors = pygame.sprite.Group()
+
+        self.room = Room(self, (WIN_WIDTH-ROOM_WIDHT)//2, (WIN_HEIGHT-ROOM_HEIGHT)//2)
+        self.room_door = RoomDoor(self, self.room, 'down')
+        self.all_sprites.add(self.room)
+        self.doors.add(self.room_door)
+        self.all_sprites.add(self.room_door)
 
         self.draw_player()
+    
+    def interact_with_door(self, door):
+        if self.player.door_side == "top":
+            # Posiciona o jogador abaixo da porta e altera o estado para "bottom"
+            self.player.rect.x = door.rect.centerx - self.player.rect.width // 2  # Centraliza o jogador na porta
+            self.player.rect.y = door.rect.bottom + 10  # Coloca o jogador logo abaixo da porta
+            self.player.door_side = "bottom"  # Muda o estado do jogador para "bottom"
+
+            # Cria e desenha o Hall
+            self.hall = Hall(self, self.room.rect.topleft[0], self.room.rect.topleft[1] + 220)  # Coloca o Hall abaixo do Room
+            self.all_sprites.add(self.hall)  # Adiciona o novo ambiente (Hall) aos sprites
+
+            # Muda o quarto atual de Room para Hall
+            if hasattr(self, 'room'):  # Verifica se a Room ainda existe
+                self.all_sprites.remove(self.room)  # Remove o quarto atual
+                self.room = None  # Limpa a referência à sala atual
+
+
+        elif self.player.door_side == "bottom":
+            # Posiciona o jogador acima da porta e altera o estado para "top"
+            self.player.rect.x = door.rect.centerx - self.player.rect.width // 2  # Centraliza o jogador na porta
+            self.player.rect.y = door.rect.top - self.player.rect.height - 10  # Coloca o jogador logo acima da porta
+            self.player.door_side = "top"  # Muda o estado do jogador para "top"
+
+            # Muda de volta para o Room
+            if hasattr(self, 'hall'):  # Verifica se o Hall existe
+                self.all_sprites.remove(self.hall)  # Remove o corredor
+                self.hall = None  # Limpa a referência ao corredor
+
+            # Cria e desenha o Room
+            self.room = Room(self, (WIN_WIDTH - ROOM_WIDHT) // 2, (WIN_HEIGHT - ROOM_HEIGHT) // 2)
+            self.all_sprites.add(self.room)  # Adiciona o novo ambiente (Room) aos sprites
     
     def events(self):
         # game loop events
@@ -33,16 +69,27 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_e:
+                    # Verifica se o jogador está perto de alguma porta
+                    for door in self.doors:
+                        if door.rect.colliderect(self.player.rect):  # Verifica se o jogador está dentro do alcance da porta
+                            self.interact_with_door(door)
+            
+
 
     def update(self):
         # game loop updates
         self.all_sprites.update()
         self.camera.box_target_camera(self.player)
+        self.camera.keyboard_control()
 
     def draw(self):
         # game loop draw
         self.screen.fill((35, 14, 13))
-        self.all_sprites.draw(self.screen)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, sprite.rect.topleft - self.camera.offset)
         self.clock.tick(FPS)
         pygame.display.update()
 
@@ -65,21 +112,28 @@ class Camera:
         self.game = game
         self.camera_rect = pygame.Rect(0, 0, width, height)
         self.offset = pygame.Vector2(0, 0)
-        self.camera_borders = {'left': 100, 'right': 100, 'top': 50, 'bottom': 50}
+        self.camera_borders = {'left': 100, 'right': 100, 'top': 200, 'bottom': 200}
         self.keyboard_speed = 5
 
     def box_target_camera(self, target):
-        if target.rect.left < self.camera_rect.left:
-            self.camera_rect.left = target.rect.left
-        if target.rect.right > self.camera_rect.right:
-            self.camera_rect.right = target.rect.right
-        if target.rect.top < self.camera_rect.top:
-            self.camera_rect.top = target.rect.top
-        if target.rect.bottom > self.camera_rect.bottom:
-            self.camera_rect.bottom = target.rect.bottom
+        if target.rect.left < self.camera_rect.left + self.camera_borders['left']:
+            self.camera_rect.left = target.rect.left - self.camera_borders['left']
 
-        self.offset.x = self.camera_rect.left - self.camera_borders['left']
-        self.offset.y = self.camera_rect.top - self.camera_borders['top']
+        # Verificar se o jogador ultrapassou a borda direita da tela
+        if target.rect.right > self.camera_rect.right - self.camera_borders['right']:
+            self.camera_rect.right = target.rect.right + self.camera_borders['right']
+
+        # Verificar se o jogador ultrapassou a borda superior da tela
+        if target.rect.top < self.camera_rect.top + self.camera_borders['top']:
+            self.camera_rect.top = target.rect.top - self.camera_borders['top']
+
+        # Verificar se o jogador ultrapassou a borda inferior da tela
+        if target.rect.bottom > self.camera_rect.bottom - self.camera_borders['bottom']:
+            self.camera_rect.bottom = target.rect.bottom + self.camera_borders['bottom']
+
+        # Atualiza o deslocamento da câmera para o movimento da tela
+        self.offset.x = self.camera_rect.left 
+        self.offset.y = self.camera_rect.top
 
     def keyboard_control(self):
         keys = pygame.key.get_pressed()
@@ -88,8 +142,8 @@ class Camera:
         if keys[pygame.K_w]: self.camera_rect.y -= self.keyboard_speed
         if keys[pygame.K_s]: self.camera_rect.y += self.keyboard_speed
 
-        self.offset.x = self.camera_rect.left - self.camera_borders['left']
-        self.offset.y = self.camera_rect.top - self.camera_borders['top']
+        self.offset.x = self.camera_rect.left
+        self.offset.y = self.camera_rect.top
 
 g = Game()
 g.intro_screen()
--- src/sprites.py
+++ src/sprites.py
@@ -9,7 +9,7 @@ class SpriteSheet():
 
     def get_sprite(self, x, y, widht, height):
         sprite = pygame.Surface([widht, height])
-        sprite.blit(self.sheet, (0,0), (x, y, widht, height))
+        sprite.blit(self.sheet, (0,0), (x*widht, y*height, widht, height))
         sprite.set_colorkey(BLACK)
         return sprite
 
@@ -23,8 +23,8 @@ class Player(pygame.sprite.Sprite):
 
         self.x = x
         self.y = y
-        self.width = TILESIZE
-        self.height = TILESIZE
+        self.width = 48
+        self.height = 57
 
         self.x_change = 0
         self.y_change = 0
@@ -32,15 +32,17 @@ class Player(pygame.sprite.Sprite):
         self.facing = 'down'
         self.animation_loop = 1
 
-        self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
+        self.image = self.game.character_spritesheet.get_sprite(1, 0, self.width, self.height)
         
-        self.image_hitbox = pygame.image.load('C:\\Users\\guguo\\OneDrive\\Área de Trabalho\\FGV\\LP\\pygame\\newsun\\img\\player_hitbox.png').convert_alpha()
+        self.image_hitbox = pygame.image.load('./img/player_hitbox.png').convert_alpha()
 
         self.mask = pygame.mask.from_surface(self.image_hitbox)
 
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+
+        self.door_side = "top"
     
     def update(self):
         previous_x = self.rect.x
@@ -52,12 +54,12 @@ class Player(pygame.sprite.Sprite):
 
         # Tentar mover horizontalmente, se possível
         self.rect.x += self.x_change
-        if self.colisao_com_paredes_horizontal(self.game.all_sprites):
+        if self.colisao_com_paredes(self.game.all_sprites):
             self.rect.x = previous_x  # Reverter o movimento horizontal se houver colisão
 
         # Tentar mover verticalmente, se possível
         self.rect.y += self.y_change
-        if self.colisao_com_paredes_vertical(self.game.all_sprites):
+        if self.colisao_com_paredes(self.game.all_sprites):
             self.rect.y = previous_y  # Reverter o movimento vertical se houver colisão
 
         # Resetar as mudanças no movimento após cada atualização
@@ -65,25 +67,25 @@ class Player(pygame.sprite.Sprite):
         self.y_change = 0
 
     def animate(self):
-        down_animations = [self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height),
-                           self.game.character_spritesheet.get_sprite(35, 2, self.width, self.height),
-                           self.game.character_spritesheet.get_sprite(68, 2, self.width, self.height)]
+        down_animations = [self.game.character_spritesheet.get_sprite(1, 0, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(0, 0, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(2, 0, self.width, self.height)]
 
-        up_animations = [self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height),
-                         self.game.character_spritesheet.get_sprite(35, 34, self.width, self.height),
-                         self.game.character_spritesheet.get_sprite(68, 34, self.width, self.height)]
+        up_animations = [self.game.character_spritesheet.get_sprite(1, 3+1/self.height, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(0, 3+1/self.height, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(2, 3+1/self.height, self.width, self.height)]
 
-        left_animations = [self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height),
-                           self.game.character_spritesheet.get_sprite(35, 98, self.width, self.height),
-                           self.game.character_spritesheet.get_sprite(68, 98, self.width, self.height)]
+        left_animations = [self.game.character_spritesheet.get_sprite(1, 1, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(0, 1, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(2, 1, self.width, self.height)]
 
-        right_animations = [self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height),
-                            self.game.character_spritesheet.get_sprite(35, 66, self.width, self.height),
-                            self.game.character_spritesheet.get_sprite(68, 66, self.width, self.height)]
+        right_animations = [self.game.character_spritesheet.get_sprite(1, 2+1/self.height, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(0, 2+1/self.height, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(2, 2+1/self.height, self.width, self.height)]
         
         if self.facing == 'down':
             if self.y_change == 0:
-                self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
+                self.image = self.game.character_spritesheet.get_sprite(1, 0, self.width, self.height)
             else:
                 self.image = down_animations[math.floor(self.animation_loop)]
                 self.animation_loop += 0.1
@@ -92,7 +94,7 @@ class Player(pygame.sprite.Sprite):
         
         if self.facing == 'up':
             if self.y_change == 0:
-                self.image = self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height)
+                self.image = self.game.character_spritesheet.get_sprite(1, 3+1/self.height, self.width, self.height)
             else:
                 self.image = up_animations[math.floor(self.animation_loop)]
                 self.animation_loop += 0.1
@@ -101,7 +103,7 @@ class Player(pygame.sprite.Sprite):
         
         if self.facing == 'left':
             if self.x_change == 0:
-                self.image = self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height)
+                self.image = self.game.character_spritesheet.get_sprite(1, 1, self.width, self.height)
             else:
                 self.image = left_animations[math.floor(self.animation_loop)]
                 self.animation_loop += 0.1
@@ -110,7 +112,7 @@ class Player(pygame.sprite.Sprite):
         
         if self.facing == 'right':
             if self.x_change == 0:
-                self.image = self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height)
+                self.image = self.game.character_spritesheet.get_sprite(1, 2+1/self.height, self.width, self.height)
             else:
                 self.image = right_animations[math.floor(self.animation_loop)]
                 self.animation_loop += 0.1
@@ -141,26 +143,17 @@ class Player(pygame.sprite.Sprite):
             self.facing = 'down'
 
 
-    def colisao_com_paredes_horizontal(self, outros_sprites):
+    def colisao_com_paredes(self, outros_sprites):
         for sprite in outros_sprites:
-            if isinstance(sprite, Quarto):  # Verifique colisão com o fundo do quarto
-                # Verifique colisões horizontais
+            if not isinstance(sprite, Player):  # Verifique colisão com o fundo do quarto
+                # Verifique colisões
                 offset = (self.rect.x - sprite.rect.x, self.rect.y - sprite.rect.y)
                 if sprite.mask.overlap(self.mask, offset):
                     return True  # Colidiu com a parede horizontalmente
         return False
 
-    def colisao_com_paredes_vertical(self, outros_sprites):
-        for sprite in outros_sprites:
-            if isinstance(sprite, Quarto):  # Verifique colisão com o fundo do quarto
-                # Verifique colisões verticais
-                offset = (self.rect.x - sprite.rect.x, self.rect.y - sprite.rect.y)
-                if sprite.mask.overlap(self.mask, offset):
-                    return True  # Colidiu com a parede verticalmente
-        return False
 
-
-class Quarto(pygame.sprite.Sprite):
+class Room(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.game = game
         self._layer = GROUND_LAYER
@@ -170,13 +163,13 @@ class Quarto(pygame.sprite.Sprite):
         self.x = x 
         self.y = y 
         self.width = ROOM_WIDHT
-        self.height = ROOM_HEIGH
+        self.height = ROOM_HEIGHT
 
-        self.image = pygame.image.load('C:\\Users\\guguo\\OneDrive\\Área de Trabalho\\FGV\\LP\\pygame\\newsun\\img\\corridor_full.png').convert_alpha()
+        self.image = pygame.image.load('./img/room_full.png').convert_alpha()
         self.image.set_colorkey(BLUE)
 
-        #self.image_colision = pygame.image.load('C:\\Users\\guguo\\OneDrive\\Área de Trabalho\\FGV\\LP\\pygame\\newsun\\img\\room_colision.png').convert_alpha()
-        #self.image_colision.set_colorkey(BLUE)
+        self.image_colision = pygame.image.load('./img/room_colision.png').convert_alpha()
+        self.image_colision.set_colorkey(BLUE)
 
         self.mask = pygame.mask.from_surface(self.image_colision)
 
@@ -187,4 +180,51 @@ class Quarto(pygame.sprite.Sprite):
         
     def update(self):
         # Atualiza o sprite do quarto se necessário
+        pass
+
+class Hall(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = GROUND_LAYER
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x 
+        self.y = y 
+        self.width =  HALL_WIDHT # Largura do corredor
+        self.height = HALL_HEIGHT  # Altura do corredor
+
+        self.image = pygame.image.load('./img/hall_full.png').convert_alpha()
+        self.image.set_colorkey(BLUE)
+
+        self.image_colision = pygame.image.load('./img/hall_colision.png').convert_alpha()
+        self.image_colision.set_colorkey(BLUE)
+
+        self.mask = pygame.mask.from_surface(self.image_colision)
+
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+    def update(self):
+        # Atualiza o sprite do corredor se necessário
+        pass
+
+class RoomDoor(pygame.sprite.Sprite):
+    def __init__(self, game, room, destination_direction):
+        super().__init__()
+        self.game = game
+        self.image = pygame.Surface((54, 9))
+        self.image.fill((255, 255, 0))  # Cor amarela para visualização da porta
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.Mask(self.image.get_size(), fill=False)
+
+        # Posicionando a porta no lado inferior do quarto
+        self.rect.topleft = (room.rect.topleft[0] + 688, room.rect.topleft[1] + 400)
+
+        self.destination_side = destination_direction  # Direção para o outro lado da porta (ex: "esquerda" ou "direita")
+
+    def update(self):
+        # Aqui podemos atualizar algo relacionado à porta, se necessário
         pass
